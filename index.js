18339f0b9ddab39bdf86f9e506e8d27831198f7c
Suguru Motegi <suguru.motegi@gmail.com>
2017-12-07 09:41:08 -0800

    refactor regexp

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ function resolveFunction(code, args) {
 
   if (/^async\s/.test(code)) {
     str = `\n    ${str}`
-      .replace(/(\n\s{4}var\s([^([|{)].*?))\s=(.*)/g, '$1 = this.$2 =$3')
+      .replace(/^(\s{4}var\s([^([|{)].*?))\s=(.*)/mg, '$1 = this.$2 =$3')
       .replace(/(async)?\s?function\s(.+)\(/g, 'this.$2 = $2;$1 function $2(');
     str = `(async () => {${str}})();`;
   }
